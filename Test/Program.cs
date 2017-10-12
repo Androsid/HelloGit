57e4f38175b4e2c949a0f6e89d8201ab5aa01006
Андрей Сидоров <asidorov@ivanovo.indusoft.ru>
2017-10-12 09:52:22 +0300

    Добавьте файлы проекта.

---
+++ Test/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //    int[] numbers = { -5, -4, 1, 3, 9, 8, 6, 7, 2 };
+            //int oddNumbers = numbers.Count(x => x % 2 == 0); // количество четных чисел в массиве
+            //Console.WriteLine(oddNumbers);
+
+
+            //  int number = numbers.First(x => x > 0); // получение первого положительного числа в массиве
+            //  int zz = numbers.ToList().IndexOf(number); // получение его индекса
+            //  Console.WriteLine(number);
+            //  Console.WriteLine(zz);
+
+
+            // Array.Sort(numbers);  // сортировка массива
+            // Array.Reverse(numbers); // обратная сортировка
+
+
+            //    var orderedNumbers = from i in numbers
+            //                         orderby i descending // по убыванию  
+            //                       //  orderby i ascending // по возрастанию
+            //                         select i;
+
+            //    foreach (int i in orderedNumbers)
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            string str = "Hello";
+            char[] tmp = str.ToCharArray();
+            Array.Reverse(tmp);
+            str = String.Join("",tmp);
+            
+            Console.Write(str);
+           
+
+            Console.ReadKey();
+        }
+    }
+}
