b163bc184bc4efbbacd397b4800c7435dce4bb12
Андрей Сидоров <Andro_oi@mail.ru>
2017-10-12 10:09:25 +0300

    Add Class AndroClass with enum

---
+++ Test/AndroClass.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    class AndroClass
+    {
+        enum models {Honda, BMW, LADA, TESLA };
+    }
+}
